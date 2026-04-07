12dd7163be35ba4a011e105bf3dd358805f132b6
cesar <cesarbarraza13@gmail.com>
2026-04-07 01:18:07 -0500

    expanded and organized

--- settings.py
+++ settings.py
@@ -1,2 +1,20 @@
-#AI model used by ai_npc.py so users can converse with NPCs
+#ai model settings; see https://github.com/ollama/ollama/blob/main/docs/modelfile.mdx#valid-parameters-and-values for a description of each option
 ai_model = 'deepseek-r1:8b'
+ai_temp = float(0.1)
+ai_num_ctx = int(2048)
+ai_repeat_last_n = int(64)
+ai_repeat_penalty = float(1.1)
+ai_seed = int(0)
+ai_stop = 'string'
+ai_num_predict = int(-1)
+ai_top_k = int(40)
+ai_top_p = float(0.9)
+ai_min_p = float(0.0)
+
+#settings 2
+
+#settings 3
+
+#settings 4
+
+#settings 5
